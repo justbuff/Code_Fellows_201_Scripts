42d9afc12e5c4a4bb12b240fc0edb7cbd6b95e88
Justin Buffington <justindbuffington@gmail.com>
2022-07-26 20:52:41 -0500

    added ops challenge 02

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# ops-challenges
+# Table of Contents
+
+- [Ops Challenge 02](challenge-02.sh)
---
+++ challenge-02.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 02 Ops Challenge Solution
+# Author: Justin Buffington
+# Date of latest revision: 7/26/2022
+# Purpose: Print a string to the terminal
+
+# Main
+
+echo "I am working perfectly"
+
+# End
