c7a620ab96ba4a395799f4628e5f3a938260be51
justbuff <justindbuffington@gmail.com>
2022-07-27 17:47:26 -0500

    created challenge-03

---
+++ challenge-03.sh
@@ -0,0 +1,11 @@
+#!/bin/bash/
+
+# Script: Ops 201 Class 03 Ops Challenge Solution
+# Author: Justin Buffington
+# Date of latest revision: 7/27/2022
+# Purpose: Print the login history of users on this computer
+
+# Main
+
+
+# End
