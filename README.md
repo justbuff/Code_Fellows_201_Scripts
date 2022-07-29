6fa5a2548362db8833659b3d111fbb51422ae553
justbuff <justindbuffington@gmail.com>
2022-07-28 22:49:53 -0500

    added challenge 04

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Table of Contents
 
 - [Ops Challenge 02](challenge-02.sh)
+- [Ops Challenge 03](challenge-03.sh)
+- [Ops Challenge 04](challenge-04.sh)
---
+++ challenge-04.sh
@@ -0,0 +1,29 @@
+#!/bin/bash/
+
+# Script: Ops 201 Class 04 Ops Challenge Solution
+# Author: Justin Buffington
+# Date of latest revision: 7/28/2022
+# Purpose: Create 4 directories with a .txt file in each
+
+# Main
+
+function dir {
+    mkdir "$1"
+}
+
+dir dir1
+dir dir2
+dir dir3
+dir dir4
+
+directories=(dir1 dir2 dir3 dir4)
+
+for x in "${directories[@]}"
+do  
+touch ./dir1/file.txt 
+touch ./dir2/file.txt
+touch ./dir3/file.txt
+touch ./dir4/file.txt
+done
+
+# End
