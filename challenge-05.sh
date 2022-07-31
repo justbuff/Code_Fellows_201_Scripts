60fd808270c8841dad6d53721a9c1e6cd41180e6
Justin Buffington <108829519+justbuff@users.noreply.github.com>
2022-07-31 16:42:43 -0500

    Update challenge-05.sh

--- challenge-05.sh
+++ challenge-05.sh
@@ -3,7 +3,7 @@
 # Script: Ops 201 Class 05 Ops Challenge Solution
 # Author: Justin Buffington
 # Date of latest revision: 7/29/2022
-# Purpose: Print running system processes, record user response, and kill selected process
+# Purpose: Print running system processes, record user response, and end selected process
 
 # Main
 
@@ -13,7 +13,7 @@ echo "Which process would you like to end?"
 
 read response
 
-For x in ($response)
+For x in $response
 do
     kill -9 x
 done
