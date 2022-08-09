d203e6f160c257713cca15eb85c0aced7fb976b3
justbuff <justindbuffington@gmail.com>
2022-08-09 00:56:22 -0500

    added challenge 10

--- challenge-10.ps1
+++ challenge-10.ps1
@@ -1,15 +1,23 @@
 # Print to the terminal screen all active processes ordered by highest CPU time consumption at the top.
-Get-Process | Where-Object { $_.CPU }
+Get-Process | Sort -Descending CPU
 
 # Print to the terminal screen all active processes ordered by highest Process Identification Number at the top.
-Get-Process 
+Get-Process | Sort -Descending Id
 
 # Print to the terminal screen the top five active processes ordered by highest Working Set (WS(K)) at the top.
+Get-Process | Sort -Descending WS | Select -First 5
 
 # Start the process Internet Explorer (iexplore.exe) and have it open https://owasp.org/www-project-top-ten/.
+[system.Diagnostics.Process]::Start("iexplore","https://owasp.org/www-project-top-ten/")
 
 # Start the process Internet Explorer (iexplore.exe) ten times using a for loop. Have each instance open https://owasp.org/www-project-top-ten/.
+$url = @("https://owasp.org/www-project-top-ten/")
+for($i = 1; $i -le 10; $1++){
+    Start-Process iexplore.exe $url
+}
 
 # Close all Internet Explorer windows.
+Get-Process iexplore | stop-process
 
-# Kill a process by its Process Identification Number. Choose a process whose termination won’t destabilize the system, such as Internet Explorer or MS Edge.
+# Kill a process by its Process Identification Number. Choose a process whose termination won’t destabilize the system, such as Internet Explorer or MS Edge. (Notepad)
+taskkill /pid 15720
