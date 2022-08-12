4d7da84241123d477b039368a03dc29561541e83
Justin Buffington <108829519+justbuff@users.noreply.github.com>
2022-08-11 22:21:43 -0500

    Update challenge-08.bat

--- challenge-08.bat
+++ challenge-08.bat
@@ -1,1 +1,10 @@
+# Script: Ops 201 Class 08 Ops Challenge Solution
+# Author: Justin Buffington
+# Date of latest revision: 8/3/2022
+# Purpose: Creates a bat file that copies a file between two locations
+
+# Main
+
 robocopy "C:\Users\j.thompson\Desktop\My Project Files" "E:\My Project Files Copy" /e
+
+# End
